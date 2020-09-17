24fecbaa7d7bd5a0ff8517c3c6e37d1e77f5281b
Giuslock <paolilligiuseppe@gmail.com>
2020-09-17 16:16:06 +0200

    login parte 2

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::view('/home', 'home');
 
 Route::get('/', function() {
 
