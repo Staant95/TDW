c37bd436be9649f090207f608af22f8583af49be
Staant95 <antokhi.stas@gmail.com>
2020-10-09 13:41:26 +0200

    added message when user does not have any orders

--- resources/views/profile/orders.blade.php
+++ resources/views/profile/orders.blade.php
@@ -4,27 +4,44 @@
 
 @section('panel-content')
 
-    @foreach ($orders as $order)
-        
-        <div class="app-order">
+    @if ($orders->count())
 
-            <div class="card" style="height: 100%">
-                <img 
-                src="{{ asset('storage/box.svg') }}" 
-                class="card-img-top" 
-                alt=""
-                style="width: 240px; height: 120px; margin: 10px auto;"
-                >
-                <div class="card-body">
-                <h5 class="card-title">Order#: {{ $order->code }}</h5>
-                <p class="card-text"> Expected day of delivery: <strong>{{ $order->expected }}</strong> </p>
-                <p class="card-text"> Shipping company: <strong> Bartolini </strong></p>
-                <p>Shipping address: <strong> {{ $order->address->street }} </strong> </p>
+      
+        @foreach ($orders as $order)
+            
+            <div class="app-order">
+
+                <div class="card" style="height: 100%">
+                    <img 
+                    src="{{ asset('storage/box.svg') }}" 
+                    class="card-img-top" 
+                    alt=""
+                    style="width: 240px; height: 120px; margin: 10px auto;"
+                    >
+                    <div class="card-body">
+                    <h5 class="card-title">Order#: {{ $order->code }}</h5>
+                    <p class="card-text"> Expected day of delivery: <strong>{{ $order->expected }}</strong> </p>
+                    <p class="card-text"> Shipping company: <strong> Bartolini </strong></p>
+                    <p>Shipping address: <strong> {{ $order->address->street }} </strong> </p>
+                    </div>
                 </div>
             </div>
-        </div>
 
-    @endforeach
+        @endforeach
+
+    @else
+        
+        <p style="grid-column: 1/4; grid-row: 2/3; font-size: 3em;">Take a look at 
+            <a 
+            href="{{ route('category.index', ['category' => 9]) }}"
+            id="app-link"
+            >
+                Today's deals
+            </a>  
+        </p>  
+
+    @endif
+
         
 
     
