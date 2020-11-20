3f9632b404763c3d81d5f2d638dcf0bf075c8256
Staant95 <antokhi.stas@gmail.com>
2020-11-20 12:41:02 +0100

    changing category and product seeders

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -6,49 +6,139 @@ use Faker\Generator as Faker;
 
 class CategoryTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    private $categoriesAndProducts = [
+        'Camicie' => [
+          'camicia',
+          'camicia',
+          'camicia beige',
+          'camicetta',
+          'camicetta',
+          'marshall shirt'
+        ],
+      
+      
+        'Hats' => [
+          'barett',
+          'im leoparden',
+          'zum wenden',
+          'watch hat',
+          'berretto',
+          'berretto'
+        ],
+      
+      
+        'Gloves' => [
+          'watch gloves',
+          'jack montana',
+          'sphere running',
+          'unisex',
+          'ottawa',
+          'gloves'
+        ],
+      
+        'Jeans' => [
+          'jeans skinny_fit',
+          'mit graffiti',
+          'mit hohem_bund',
+          'mit rissen_large.jpg',
+          'jeans skinny_fit',
+          'jeans tapered_fit'
+        ],
+      
+        'Shoes' => [
+          'stan smith',
+          'sneakers large.jpg',
+          'max aura',
+          'air force_1',
+          'rsx',
+          'smash v2'
+        ],
+      
+        'Skirt' => [
+          'highrise',
+          'shorts',
+          'gonna jeans',
+          'nukaty skirt',
+          'rock im_koko',
+          'gonna jeans'
+        ],
+      
+        'Socks' => [
+          'midcut',
+          'solid crew',
+          'jacjens',
+          'regular',
+          'kneehigh',
+          'sock checks'
+        ],
+       'Sport' => [ 
+           'lin', 
+           'tuta', 
+           'tuta', 
+           'crew logo', 
+           'taino', 
+           'collant' 
+       ],
+
+        'Suits' => [
+          'cappotto corto',
+          'nuamaury',
+          'giacca',
+          'jprstuart',
+          'onlysmark',
+          'giacca'
+        ],
+
+        'Tshirt' => [
+          'polo black',
+          'tshirt',
+          'tshirt',
+          'cutandsew tee',
+          'contrast pocket_tee',
+          'edie'
+        ],
+
+        'Sunglasses' => [
+          'esky',
+          'sunglasses',
+          'sunglasses',
+          'jackpot',
+          'likoma',
+          'sunglasses'
+        ],
+        
+        'Underwear' => [
+          'modern bralette',
+          'boxer',
+          'plunge pushup',
+          'boxer',
+          'slip',
+          'basic thong',
+          'slip',
+          'perizoma'
+        ]
+      ];
+
+
+
     public function run(Faker $faker)
     {
-        $categories = collect([
-            Category::create(['name' => 'Deals']), // 1-8
-            Category::create(['name' => "Women"]), // 9-16
-            Category::create(['name' => "Men"]),   // 17-24
-            Category::create(['name' => "Jeans"]), // 25-32
-            Category::create(['name' => "Shoes"]), // 32-40
-            Category::create(['name' => "T-shirt"]), //41-48
-            Category::create(['name' => "Sunglasses"]),//49-56
-            Category::create(['name' => "Underwear"]),//57-64
-            Category::create(['name' => "Sport"]),//65-72
-            Category::create(['name' => "Kids"]), // 73-80
-            // Category::create(['name' => "Summer Sales"]),
-            // Category::create(['name' => "Winter Sales"]),
-            // Category::create(['name' => "Trending"]),
-            Category::create(['name' => 'Skirt']), // 81-88
-            Category::create(['name' => "Hats"]), // 89-96
-            Category::create(['name' => "Socks"]), // 97-104
-            Category::create(['name' => "Gloves"]), // 105-112
-            Category::create(['name' => 'Men-Suits']), // 113-120
-        ]);
+        
+        Category::create(['name' => 'Camicie']); // 1-8
+        Category::create(['name' => "Gloves"]); // 9-16
+        Category::create(['name' => "Hats"]);  // 17-24
+        Category::create(['name' => "Jeans"]); // 25-32
+        Category::create(['name' => "Shoes"]); // 32-40
+        Category::create(['name' => "Skirt"]); //41-48
+        Category::create(['name' => "Scoks"]);//49-56
+        Category::create(['name' => "Sport"]);//57-64
+        Category::create(['name' => "Suits"]);//65-72
+        Category::create(['name' => "Sunglasses"]); // 73-80
+        Category::create(['name' => 'Tshirt']); // 81-88
+        Category::create(['name' => "Underwear"]); // 89-96
+         
 
-        $products = App\Product::all();
 
-        $arrOfProducts = $products->chunk(8);
-        $indexArray = 0;
-
-        // itero su ogni categoria, 
-       foreach($categories as $category) {
-            
-            foreach($arrOfProducts[$indexArray] as $product) {
-
-                $category->products()->attach($product->id);
-
-            }
-
-            $indexArray += 1;
-       }
+        
     }
 }
--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -12,18 +12,6 @@ class ProductTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        factory(App\Product::class, 120) // categories * chunks(8)
-        ->create()
-        ->each(function($product) use($faker) {
-
-            $formatSize = App\Format::count();
-            $randomFormat = $faker->numberBetween(1, $formatSize);
-            $format = App\Format::find($randomFormat);
-            $product->formats()->attach(
-                $format,
-                [ 'quantity' => $faker->numberBetween(5,40)]
-            );
-
-        });
+        
     }
 }
